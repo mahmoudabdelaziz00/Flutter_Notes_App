1252aceb3e3ebe3f52d63b819120c352118cf07c
Mahmoud Abdelaziz <mahmoudabdelaziz00@gmail.com>
2025-05-03 13:25:24 +0300

    change theme and create notes view

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: const NotesApp(),
+    );
   }
 }
