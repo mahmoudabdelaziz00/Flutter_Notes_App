aa349a1b627915f2d7a1a39fb845908bc88e8b33
Mahmoud Abdelaziz <mahmoudabdelaziz00@gmail.com>
2025-05-04 08:08:48 +0300

    create notes list view

--- lib/views/widgets/custom_notes_item.dart
+++ lib/views/widgets/custom_notes_item.dart
@@ -6,6 +6,7 @@ class NotesItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(bottom: 16),
       padding: EdgeInsets.only(
         top: 24,
         bottom: 24,
