990bae630c96469ae465d97a96f8028a92b2b0c8
Mahmoud Abdelaziz <mahmoudabdelaziz00@gmail.com>
2025-05-10 13:52:37 +0300

    finish notes bottom sheet

---
+++ lib/views/widgets/custom_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constant.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+        color: kPrimaryColor,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Center(
+        child: Text(
+          'ADD',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 18
+          ),
+        ),
+      ),
+    );
+  }
+}
