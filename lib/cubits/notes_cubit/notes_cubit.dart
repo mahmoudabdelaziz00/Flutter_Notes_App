a404b32e3ad7b7d01bca9089f74be1ed6510ca1f
Mahmoud Abdelaziz <mahmoudabdelaziz00@gmail.com>
2025-05-12 16:58:56 +0300

    create fetch notes method

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/models/note_model.dart';
+
+import '../../constant.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  fetchAllNotes(NoteModel note) async {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> note = notesBox.values.toList();
+      emit(NotesSuccess(note));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
